9ac0a1471a32911056f98b80784c9cbb581f74f2
lukieboiz <luke_a_jones@hotmail.com>
2018-04-18 15:41:42 +0100

    Add files via upload

---
+++ calendarplannerexample/CalendarPlannerExample/CalendarPlannerExample/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace CalendarPlannerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            WeekPlanner.DataColumns dataColumns3 = new WeekPlanner.DataColumns();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_MinusDay = new System.Windows.Forms.Button();
+            this.btn_PlusDay = new System.Windows.Forms.Button();
+            this.btn_DeleteRows = new System.Windows.Forms.Button();
+            this.btn_RemoveRow = new System.Windows.Forms.Button();
+            this.btn_SetItemDate = new System.Windows.Forms.Button();
+            this.btn_ClearColumn = new System.Windows.Forms.Button();
+            this.btn_DeleteItem = new System.Windows.Forms.Button();
+            this.btn_NewItem = new System.Windows.Forms.Button();
+            this.btn_AddRow = new System.Windows.Forms.Button();
+            this.weekPlanner = new WeekPlanner.CalendarPlanner();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_MinusDay);
+            this.panel1.Controls.Add(this.btn_PlusDay);
+            this.panel1.Controls.Add(this.btn_DeleteRows);
+            this.panel1.Controls.Add(this.btn_RemoveRow);
+            this.panel1.Controls.Add(this.btn_SetItemDate);
+            this.panel1.Controls.Add(this.btn_ClearColumn);
+            this.panel1.Controls.Add(this.btn_DeleteItem);
+            this.panel1.Controls.Add(this.btn_NewItem);
+            this.panel1.Controls.Add(this.btn_AddRow);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(5, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(684, 100);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // comboBox
+            // 
+            this.comboBox.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "G218",
+            "H039",
+            "H213",
+            "J202/204",
+            "J210",
+            "J255",
+            "J325"});
+            this.comboBox.Location = new System.Drawing.Point(589, 43);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox.Size = new System.Drawing.Size(90, 21);
+            this.comboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(551, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Room:";
+            // 
+            // btn_MinusDay
+            // 
+            this.btn_MinusDay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_MinusDay.Location = new System.Drawing.Point(589, 4);
+            this.btn_MinusDay.Name = "btn_MinusDay";
+            this.btn_MinusDay.Size = new System.Drawing.Size(41, 23);
+            this.btn_MinusDay.TabIndex = 29;
+            this.btn_MinusDay.Text = "Day -";
+            this.btn_MinusDay.UseVisualStyleBackColor = true;
+            this.btn_MinusDay.Click += new System.EventHandler(this.btn_MinusDay_Click);
+            // 
+            // btn_PlusDay
+            // 
+            this.btn_PlusDay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_PlusDay.Location = new System.Drawing.Point(636, 4);
+            this.btn_PlusDay.Name = "btn_PlusDay";
+            this.btn_PlusDay.Size = new System.Drawing.Size(43, 23);
+            this.btn_PlusDay.TabIndex = 28;
+            this.btn_PlusDay.Text = "Day +";
+            this.btn_PlusDay.UseVisualStyleBackColor = true;
+            this.btn_PlusDay.Click += new System.EventHandler(this.btn_PlusDay_Click);
+            // 
+            // btn_DeleteRows
+            // 
+            this.btn_DeleteRows.Location = new System.Drawing.Point(168, 4);
+            this.btn_DeleteRows.Name = "btn_DeleteRows";
+            this.btn_DeleteRows.Size = new System.Drawing.Size(110, 23);
+            this.btn_DeleteRows.TabIndex = 26;
+            this.btn_DeleteRows.Text = "Delete rows";
+            this.btn_DeleteRows.UseVisualStyleBackColor = true;
+            this.btn_DeleteRows.Click += new System.EventHandler(this.btn_DeleteRows_Click);
+            // 
+            // btn_RemoveRow
+            // 
+            this.btn_RemoveRow.Location = new System.Drawing.Point(87, 4);
+            this.btn_RemoveRow.Name = "btn_RemoveRow";
+            this.btn_RemoveRow.Size = new System.Drawing.Size(75, 23);
+            this.btn_RemoveRow.TabIndex = 24;
+            this.btn_RemoveRow.Text = "Delete Row";
+            this.btn_RemoveRow.UseVisualStyleBackColor = true;
+            this.btn_RemoveRow.Click += new System.EventHandler(this.btn_RemoveRow_Click);
+            // 
+            // btn_SetItemDate
+            // 
+            this.btn_SetItemDate.Location = new System.Drawing.Point(168, 32);
+            this.btn_SetItemDate.Name = "btn_SetItemDate";
+            this.btn_SetItemDate.Size = new System.Drawing.Size(97, 23);
+            this.btn_SetItemDate.TabIndex = 23;
+            this.btn_SetItemDate.Text = "Set Item Date";
+            this.btn_SetItemDate.UseVisualStyleBackColor = true;
+            this.btn_SetItemDate.Click += new System.EventHandler(this.btn_SetItemDate_Click);
+            // 
+            // btn_ClearColumn
+            // 
+            this.btn_ClearColumn.Location = new System.Drawing.Point(6, 61);
+            this.btn_ClearColumn.Name = "btn_ClearColumn";
+            this.btn_ClearColumn.Size = new System.Drawing.Size(94, 23);
+            this.btn_ClearColumn.TabIndex = 21;
+            this.btn_ClearColumn.Text = "Clear Column";
+            this.btn_ClearColumn.UseVisualStyleBackColor = true;
+            this.btn_ClearColumn.Click += new System.EventHandler(this.btn_ClearColumn_Click);
+            // 
+            // btn_DeleteItem
+            // 
+            this.btn_DeleteItem.Location = new System.Drawing.Point(87, 32);
+            this.btn_DeleteItem.Name = "btn_DeleteItem";
+            this.btn_DeleteItem.Size = new System.Drawing.Size(75, 23);
+            this.btn_DeleteItem.TabIndex = 20;
+            this.btn_DeleteItem.Text = "Delete Item";
+            this.btn_DeleteItem.UseVisualStyleBackColor = true;
+            this.btn_DeleteItem.Click += new System.EventHandler(this.btn_DeleteItem_Click);
+            // 
+            // btn_NewItem
+            // 
+            this.btn_NewItem.Location = new System.Drawing.Point(6, 32);
+            this.btn_NewItem.Name = "btn_NewItem";
+            this.btn_NewItem.Size = new System.Drawing.Size(75, 23);
+            this.btn_NewItem.TabIndex = 18;
+            this.btn_NewItem.Text = "New Item";
+            this.btn_NewItem.UseVisualStyleBackColor = true;
+            this.btn_NewItem.Click += new System.EventHandler(this.btn_NewItem_Click);
+            // 
+            // btn_AddRow
+            // 
+            this.btn_AddRow.Location = new System.Drawing.Point(6, 3);
+            this.btn_AddRow.Name = "btn_AddRow";
+            this.btn_AddRow.Size = new System.Drawing.Size(75, 23);
+            this.btn_AddRow.TabIndex = 16;
+            this.btn_AddRow.Text = "Add row";
+            this.btn_AddRow.UseVisualStyleBackColor = true;
+            this.btn_AddRow.Click += new System.EventHandler(this.btn_AddRow_Click);
+            // 
+            // weekPlanner
+            // 
+            this.weekPlanner.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(98)))), ((int)(((byte)(128)))));
+            this.weekPlanner.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.weekPlanner.Columns = dataColumns3;
+            this.weekPlanner.CurrentDate = new System.DateTime(2011, 9, 9, 14, 3, 35, 690);
+            this.weekPlanner.DatesIntervalMode = WeekPlanner.WeekPlannerMode.Daily;
+            this.weekPlanner.DayCount = 7;
+            this.weekPlanner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.weekPlanner.GridBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(221)))), ((int)(((byte)(242)))));
+            this.weekPlanner.GridCellHeight = 50;
+            this.weekPlanner.GridTextFont = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.weekPlanner.HeaderBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(244)))), ((int)(((byte)(255)))));
+            this.weekPlanner.HeaderColumnsFont = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.weekPlanner.HeaderDatesFont = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.weekPlanner.HeaderFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(192)))), ((int)(((byte)(234)))));
+            this.weekPlanner.HeaderFillLeftMarginColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(221)))), ((int)(((byte)(252)))));
+            this.weekPlanner.HeaderStyleMode = WeekPlanner.HeaderStyle.Aqua;
+            this.weekPlanner.IsAllowedDraggingBetweenRows = true;
+            this.weekPlanner.IsAllowedTreeViewDrawing = true;
+            this.weekPlanner.ItemHeight = 20;
+            this.weekPlanner.ItemTextFont = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.weekPlanner.LeftMargin = 350;
+            this.weekPlanner.LeftMarginColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(231)))), ((int)(((byte)(245)))));
+            this.weekPlanner.Location = new System.Drawing.Point(5, 105);
+            this.weekPlanner.Name = "weekPlanner";
+            this.weekPlanner.Size = new System.Drawing.Size(684, 331);
+            this.weekPlanner.TabIndex = 1;
+            this.weekPlanner.ItemDoubleClick += new WeekPlanner.CalendarPlanner.CalendarItemEventHandler(this.weekPlanner_ItemDoubleClick);
+            this.weekPlanner.RowDoubleClick += new WeekPlanner.CalendarPlanner.CalendarRowEventHandler(this.weekPlanner_RowDoubleClick);
+            this.weekPlanner.RowLeftColumnClick += new WeekPlanner.CalendarPlanner.CalendarRowLeftColumnEventHandler(this.weekPlanner_RowLeftColumnClick);
+            this.weekPlanner.Load += new System.EventHandler(this.weekPlanner_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 441);
+            this.Controls.Add(this.weekPlanner);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private WeekPlanner.CalendarPlanner weekPlanner;
+        private System.Windows.Forms.Button btn_MinusDay;
+        private System.Windows.Forms.Button btn_PlusDay;
+        private System.Windows.Forms.Button btn_DeleteRows;
+        private System.Windows.Forms.Button btn_RemoveRow;
+        private System.Windows.Forms.Button btn_SetItemDate;
+        private System.Windows.Forms.Button btn_ClearColumn;
+        private System.Windows.Forms.Button btn_DeleteItem;
+        private System.Windows.Forms.Button btn_NewItem;
+        private System.Windows.Forms.Button btn_AddRow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox;
+    }
+}
+
---
+++ calendarplannerexample/CalendarPlannerExample/CalendarPlannerExample/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeekPlanner;
+
+
+namespace CalendarPlannerExample
+{
+    public partial class Form1 : Form
+    {
+        private int day;
+
+        public Form1()
+        {
+            InitializeComponent();
+            weekPlanner.Columns.Add("Room", "Room", 150);
+            this.comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            weekPlanner.CurrentDate = DateTime.Now;
+        }
+
+        public IEnumerable<WeekPlannerItem> GetItemsByRow(int rowIndex)
+        {
+            var rows = weekPlanner.Rows;
+            var row = rows.ElementAt(rowIndex);
+            return row.Items.ToList();
+        }
+
+        private void btn_AddRow_Click(object sender, EventArgs e)
+        {
+            if(comboBox.SelectedIndex > - 1)
+            {
+                bool exists = false;
+                var itemCollection = new WeekPlannerItemCollection();
+
+                var ColumnRows = new DataColumns(weekPlanner.Calendar);
+                ColumnRows["Room"].Data.Add(comboBox.Text.ToString());
+                for (int i = 0; i < weekPlanner.RowCount; i ++)
+                {
+                    Console.Write(weekPlanner.Calendar.Columns["Room"].Data.ElementAtOrDefault(0));
+                }
+
+                if (exists)
+                {
+                    MessageBox.Show("Room already exists in a row");
+                }
+                else
+                {
+                    weekPlanner.Rows.Add(ColumnRows, itemCollection);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a room");
+            }
+        }
+
+        private void btn_AddRow2_Click(object sender, EventArgs e)
+        {
+            var ColumnRows = new DataColumns(weekPlanner.Calendar);
+
+            var itemCollection = new WeekPlannerItemCollection();
+
+            var item = new WeekPlannerItem();
+            item.StartDate = DateTime.Now.AddDays(0);
+            item.EndDate = DateTime.Now.AddDays(0);
+            item.Subject = "1 date";
+            item.BackColor = Color.YellowGreen;
+            itemCollection.Add(item);
+
+            var row = new WeekPlannerRow();
+            row.Columns = ColumnRows;
+            ColumnRows["Room"].Data.Add("J202");
+
+            row.Name = Guid.NewGuid().ToString().Substring(0, 5);
+            row.Items = itemCollection;
+            row.IsCollapsible = true;
+            weekPlanner.Rows.Add(row);
+        }
+
+        private void btn_RemoveRow_Click(object sender, EventArgs e)
+        {
+            if (weekPlanner.Rows.Count > 0)
+            {
+                if (!weekPlanner.Rows[weekPlanner.SelectedRowIndex].IsCollapsible)
+                {
+
+                    weekPlanner.Rows.Remove(weekPlanner.SelectedRowIndex);
+                }
+            }
+        }
+
+
+        private bool isSelectedRow()
+        {
+            if (weekPlanner.SelectedRowIndex == -1)
+            {
+                MessageBox.Show("Please select cell");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btn_SetRowColor_Click(object sender, EventArgs e)
+        {
+            if (isSelectedRow())
+            {
+                var row = weekPlanner.Rows.ElementAt(weekPlanner.SelectedRowIndex);
+                row.BackColor = Color.Lime;
+            }
+        }
+
+        private void btn_DeleteRows_Click(object sender, EventArgs e)
+        {
+            weekPlanner.Rows.Clear();
+        }
+
+        private void btn_NewItem_Click(object sender, EventArgs e)
+        {
+            if (isSelectedRow())
+            {
+                var rows = weekPlanner.Rows;
+                var row = rows.ElementAt(weekPlanner.SelectedRowIndex);
+
+                var item = new WeekPlannerItem();
+                item.StartDate = DateTime.Now.AddDays(0);
+                item.EndDate = DateTime.Now.AddDays(0);
+                item.Subject = "Room";
+                item.BackColor = Color.YellowGreen;
+
+                row.Items.Add(item);
+            }
+        }
+
+        private void btn_DeleteItems_Click(object sender, EventArgs e)
+        {
+            if (isSelectedRow())
+            {
+                var rows = weekPlanner.Rows;
+                var row = rows.ElementAt(weekPlanner.SelectedRowIndex);
+                row.Items.Clear();
+            }
+        }
+
+        private void btn_DeleteItem_Click(object sender, EventArgs e)
+        {
+            if (isSelectedRow())
+            {
+                var rows = weekPlanner.Rows;
+                var row = rows.ElementAt(weekPlanner.SelectedRowIndex);
+
+                var item = row.Items.Where(d => d.Subject == "Room").FirstOrDefault();
+                if (item == null)
+                {
+                    MessageBox.Show("Item is not found");
+                    return;
+                }
+                row.Items.Remove(item);
+            }
+        }
+
+        private void btn_SetItemDate_Click(object sender, EventArgs e)
+        {
+            if (isSelectedRow())
+            {
+                var rows = weekPlanner.Rows;
+                var row = rows.ElementAt(weekPlanner.SelectedRowIndex);
+
+                var item = row.Items.Where(d => d.Subject == "Room").FirstOrDefault();
+                if (item == null)
+                {
+                    MessageBox.Show("Item is not found");
+                    return;
+                }
+                item.StartDate = DateTime.Now.AddDays(1);
+                item.EndDate = DateTime.Now.AddDays(5);
+            }
+        }
+
+        private void btn_EditMode_Click(object sender, EventArgs e)
+        {
+            if (weekPlanner.SelectedItem == null)
+            {
+                MessageBox.Show("Please select an item");
+                return;
+            }
+
+            weekPlanner.ActivateEditMode(weekPlanner.SelectedItem);
+        }
+
+        private void btn_ClearColumn_Click(object sender, EventArgs e)
+        {
+            if (isSelectedRow())
+            {
+                var row = weekPlanner.Rows.ElementAt(weekPlanner.SelectedRowIndex);
+                row.Columns["Room"].Data.Clear();
+            }
+        }
+
+        private void btn_AddColumnData_Click(object sender, EventArgs e)
+        {
+            if (isSelectedRow())
+            {
+                var row = weekPlanner.Rows.ElementAt(weekPlanner.SelectedRowIndex);
+                row.Columns["Room"].Data.Add(Guid.NewGuid().ToString().Substring(0, 8), Color.Red);
+            }
+        }
+
+        private void weekPlanner_ItemDoubleClick(object sender, WeekPlannerItemEventArgs e)
+        {
+            weekPlanner.ActivateEditMode(e.Item);
+            e.Item.BackColor = Color.Yellow;
+        }
+
+        private void weekPlanner_RowDoubleClick(object sender, RowEventArgs e)
+        {
+            weekPlanner.Rows.ElementAt(weekPlanner.SelectedRowIndex).Items.Add(weekPlanner.SelectedCellDate, weekPlanner.SelectedCellDate, "Info here");
+        }
+
+        private void btn_MinusDay_Click(object sender, EventArgs e)
+        {
+            day--;
+            weekPlanner.CurrentDate = DateTime.Now.AddDays(day);
+        }
+
+        private void btn_PlusDay_Click(object sender, EventArgs e)
+        {
+            day++;
+            weekPlanner.CurrentDate = DateTime.Now.AddDays(day);
+        }
+        private void weekPlanner_RowLeftColumnClick(object sender, RowClickEventArgs e, int rowNumber)
+        {
+            if (e.MouseButton == MouseButtons.Right)
+            {
+                MessageBox.Show(e.Row.Level.ToString());
+            }
+
+            if (e.Row.IsCollapsible)
+            {
+                if (e.Row.IsExpanded)
+                {
+                    var count = IterateChilds(e.Row.Name, true, 0);
+                    //if not found child nodes
+                    if (count == 0)
+                    {
+                        for (int i = 1; i < 3; i++)
+                        {
+                            var row = new WeekPlannerRow();
+                            var ColumnRows = new DataColumns(weekPlanner.Calendar);
+                            row.Columns = ColumnRows;
+
+                            ColumnRows["Room"].Data.Add(Guid.NewGuid().ToString().Substring(0, 8));
+ 
+                            row.Name = Guid.NewGuid().ToString().Substring(0, 5);
+                            row.Level = e.Row.Level + 1;
+                            row.AncestorName = e.Row.Name;
+
+                            if (i == 2 && e.Row.Level<2)
+                            {
+                                row.IsCollapsible = true;
+                            }
+                            else
+                            {
+                                row.LeftMarginBackColor = Color.FromArgb(240, 242, 245);
+                                row.BackColor = Color.GhostWhite;
+                            }
+                            row = weekPlanner.Rows.Insert(rowNumber + i, row);
+                        }
+ 
+                    }
+                }
+                //if found child nodes
+                else
+                {
+                    var r = weekPlanner.Rows.ElementAt(rowNumber);
+                    IterateChilds(r.Name,false,0);
+                }
+            }
+
+        }
+
+        //iterates child nodes
+        private int IterateChilds(string ancestor, bool isVisible, int childCount)
+        {
+            var nodes = weekPlanner.Rows.Where(r => r.AncestorName == ancestor);
+
+            foreach (var node in nodes)
+            {
+                childCount++;
+                node.IsVisible = isVisible;
+                node.IsExpanded = true;
+                IterateChilds(node.Name, isVisible, childCount);
+            }
+            return childCount;
+        }
+
+        private void weekPlanner_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
